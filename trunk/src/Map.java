07d507f46833ca27a51848bd7192734876e37007
johannes.linke@gmx.de <johannes.linke@gmx.de@7982bf92-84ca-5f35-e2e6-f581dede5c36>
2011-08-17 11:34:25 +0000

    Karte -> Map und Raeschtschreibfelehr

---
+++ trunk/src/Map.java
@@ -0,0 +1,17 @@
+
+public class Map {
+	public Tile [][] mapFields;
+	public int width, height;
+	public Map (int [] [] mapData){
+		
+		width = mapData.length;
+		height  = mapData[0].length;
+		mapFields = new Tile[width][height];
+		for (int i = 0; i < width; i++){
+			for (int j = 0; j < width; j++){
+				mapFields[i][j]=new Tile(i,j,mapData[width][height]);
+				
+			}
+		}
+	}
+}

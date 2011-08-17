f38c172c3b4db01ed41016765baecb841a7982c2
basti-boerner@web.de <basti-boerner@web.de@7982bf92-84ca-5f35-e2e6-f581dede5c36>
2011-08-17 11:28:22 +0000

    Figure.java die zweite

--- trunk/src/Tools.java
+++ trunk/src/Tools.java
@@ -1,7 +1,13 @@
 import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 
 public class Tools {
+	
+	private static Scanner scanner;
+	private static File file;
 	
 	public static int[][] parseData(String input) {
 		String[] lines = input.split("\n");
@@ -19,5 +25,27 @@ public class Tools {
 		}
 		System.out.println(Arrays.toString(result[0]));
 		return result;
+	}
+	
+	public static String getWorld(String dateiname)
+	{	
+		String world="";
+		file = new File("rsc/"+dateiname);
+		
+		try{
+		
+			scanner = new Scanner(file); 
+			while(scanner.hasNext())
+			{
+				world= world + scanner.nextLine()+"\n";
+			}
+		}
+		
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}	
+		
+		return world;
 	}
 }

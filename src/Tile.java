6bef2fb7f7d5d6ede552deec9800d6fe4b7d894e
Michael-Decker@web.de <Michael-Decker@web.de@7982bf92-84ca-5f35-e2e6-f581dede5c36>
2011-08-17 11:22:44 +0000

    So meine Karte und die Tiles

---
+++ src/Tile.java
@@ -0,0 +1,12 @@
+
+public class Tile {
+	public int x,y;   //Koordinaten
+	public int type;  //Tiel Typ
+	
+	public Tile (int xco,  int yco, int typeData){
+		x = xco;
+		y = yco;
+		type = typeData;
+	}
+
+}

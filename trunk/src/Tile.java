f65e80075373b2ffe7853e19ba5c9c9bcdb984f0
Michael-Decker@web.de <Michael-Decker@web.de@7982bf92-84ca-5f35-e2e6-f581dede5c36>
2011-08-18 11:16:17 +0000

    Fehler in Tiles geändert und  die Image ID hinzugefügt

--- trunk/src/Tile.java
+++ trunk/src/Tile.java
@@ -1,12 +1,21 @@
 
 public class Tile {
-	public int x,y;   //Koordinaten
-	public int type;  //Tiel Typ
+	private int x,y;   //Koordinaten
+	private int type;  //Tiel Typ
+	private int  walkable;
+	private int imageID;  
 	
-	public Tile (int xco,  int yco, int typeData){
+	public Tile (int xco,  int yco, int typeData, int tile){
 		x = xco;
 		y = yco;
 		type = typeData;
+		this.imageID = tile;
+		
+		switch (type){
+	    default: walkable = 1;
+		break;
+		}
+		
 	}
 
 }

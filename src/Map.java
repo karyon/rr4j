c6e2ded4deb21afae7dd87f0afb4546a87061e40
Michael-Decker@web.de <Michael-Decker@web.de@7982bf92-84ca-5f35-e2e6-f581dede5c36>
2011-08-18 11:16:17 +0000

    Fehler in Tiles geändert und  die Image ID hinzugefügt

--- src/Map.java
+++ src/Map.java
@@ -9,7 +9,7 @@ public class Map {
 		mapFields = new Tile[width][height];
 		for (int i = 0; i < width; i++){
 			for (int j = 0; j < width; j++){
-				mapFields[i][j]=new Tile(i,j,mapData[width][height]);
+				mapFields[i][j]=new Tile(i,j,mapData[width][height],1);
 				
 			}
 		}

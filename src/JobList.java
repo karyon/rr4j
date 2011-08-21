e1b82e4c1411e2ecf694bb74975d7972869e6b54
johannes.linke@gmx.de <johannes.linke@gmx.de@7982bf92-84ca-5f35-e2e6-f581dede5c36>
2011-08-21 17:02:23 +0000

    Struktur für Jobs geschaffen, die nacheinander ausgeführt werden. RockRaiders laufen jetzt rechtsgeklickte Punkte nacheinander ab.

---
+++ src/JobList.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+
+public class JobList {
+	ArrayList<Job> list = new ArrayList<Job>();
+	
+	
+	public void addJob(Job job) {
+		list.add(job);
+		if (list.size() == 1)
+			job.execute();
+	}
+	
+	public void jobDoneExecuteNext() {
+		list.remove(0);
+		if (!list.isEmpty())
+			list.get(0).execute();
+	}
+	
+	public void jobDoneCancelNext() {
+		list = new ArrayList<Job>();
+	}
+}

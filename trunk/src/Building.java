4e7458cc2836ca81a582b0672b5070882a19c8b6
basti-boerner@web.de <basti-boerner@web.de@7982bf92-84ca-5f35-e2e6-f581dede5c36>
2011-08-22 11:52:25 +0000

    adden vergessen

---
+++ trunk/src/Building.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+
+public class Building extends GameObject{
+	
+	private static ArrayList<Building> buildingList = new ArrayList<Building>();
+	private static final int size = Tile.getSize();
+	private Image img = new BufferedImage(size, size, BufferedImage.TYPE_INT_ARGB);
+	private final int ID;
+	private static int nextID=0;
+	
+	public Building(double x, double y) {
+		super(x, y, size, size);
+		ID = nextID;
+		Graphics g = img.getGraphics();
+		g.setColor(new Color(240,170,170));
+		g.fillRect(0, 0, size-1, size-1);
+		g.setColor(Color.BLACK);
+		g.drawString(Integer.toString(ID),5, 15);
+		buildingList.add(this);
+		nextID++;
+		
+		
+		
+		
+	}
+	public static ArrayList<Building> getBuildingList(){
+		return buildingList;
+	}
+	
+	public static int getSize(){
+		return size;
+	}
+	
+	public static void paintAll(Graphics g) {
+		for (Building f : buildingList)
+			f.paint(g);
+	}
+	
+	public void paint(Graphics g) {
+		g.drawImage(img, (int) x, (int) y, null);
+	}
+
+	
+}

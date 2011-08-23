8b1d1b47f83a744c215dd4b34a9ee94b19082088
Michael-Decker@web.de <Michael-Decker@web.de@7982bf92-84ca-5f35-e2e6-f581dede5c36>
2011-08-23 12:07:38 +0000

    Res aufnehmen  (diesmal richtig)

---
+++ src/Player.java
@@ -0,0 +1,22 @@
+
+public class Player {
+	private static int ore = 0, crystal = 0;
+
+	public static int getOre() {
+		return ore;
+	}
+
+	public static void setOre(int ore) {
+		Player.ore = ore;
+	}
+
+	public static int getCrystal() {
+		return crystal;
+	}
+
+	public static void setCrystal(int crystal) {
+		Player.crystal = crystal;
+	}
+	
+
+}

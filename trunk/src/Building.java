cb12dd373287776e39cbe98e1a438ec29c8592f8
Michael-Decker@web.de <Michael-Decker@web.de@7982bf92-84ca-5f35-e2e6-f581dede5c36>
2011-08-24 11:13:57 +0000

    Res aufnehmen  (diesmal richtig)(diemal mit zurückbringen)(außerdem kommt der ToolStore als eigene Klasse ins Spiel (pun intended))

--- trunk/src/Building.java
+++ trunk/src/Building.java
@@ -3,14 +3,12 @@ import java.util.ArrayList;
 
 public class Building extends GameObject{
 	
-	private static ArrayList<Building> buildingList = new ArrayList<Building>();
-	private static final int size = Tile.getSize();
-	private final int ID;
-	private static int nextID=0;
+	protected static ArrayList<Building> buildingList = new ArrayList<Building>();
+	protected static final int size = Tile.getSize();
+	protected final int ID;
+	protected static int nextID=0;
 
-	final int toolstore = 0;
-	final int powerstation = 1;
-	private static int type;
+	protected static int type;
 	
 	public Building(double x, double y,int type) {
 		super(x, y, size, size);
---
+++ trunk/src/ToolStore.java
@@ -0,0 +1,13 @@
+
+public class ToolStore extends Building {
+
+	//protected final int ID;
+	public ToolStore(double x, double y, int type) {
+		super(x, y, type);
+
+		
+	}
+	
+	
+
+}
